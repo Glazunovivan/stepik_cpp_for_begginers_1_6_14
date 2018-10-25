6355877bc5d4a2ad05af4621b01b3b089e80575c
Ivan <43815808+Glazunovivan@users.noreply.github.com>
2018-10-25 12:56:10 +0300

    Update stepik1_16_14.cpp

--- stepik1_16_14.cpp
+++ stepik1_16_14.cpp
@@ -24,7 +24,7 @@ int main() {
 			cout<<0;
 		}*/
 		//решений беск.множество
-		if (b == 0 && d == 0 && (e / a) == (f / c)) {
+		/*if (b == 0 && d == 0 && (e / a) == (f / c)) {
 			double x_min = e / a;
 			cout << 3 << " " << x_min;
 		}
@@ -50,7 +50,69 @@ int main() {
 		{
 			cout << 0;
 		}
-		return 0;
+		return 0;*/
+		if(deltax == 0 && deltay == 0)
+       		{
+            		if(a == 0 && b==0 && c==0 && d == 0)
+            		{
+                		if(e != 0 || f!=0)
+                		{
+                    			cout<<0;
+                		}
+                		else
+                		{
+                    			cout<<5;
+                		}
+            		}
+            		else
+            		{
+               		 	if(a == 0 && c==0)
+                		{
+                    			if(b != 0)
+                    			{
+                        			y = e/b;
+                    			}
+                    			else
+                    			{
+                    	 			y = f/d;
+                    			}
+                    			cout<<4<<" "<<y;
+                		}
+                		else
+                		{
+                    			if(b==0 && d==0)
+					{
+                    				if(a!=0)
+                        			{
+                            				x = e/a;
+                        			}
+                        			else
+                        			{
+                            				x = f/c;
+                        			}
+                        			cout<<3<<" "<<x;
+                    			}
+                    			else
+                    			{
+                        			if(b != 0)
+                        			{
+                            				n = e/b;
+                            				k = -a/b;
+                        			}
+                        			else
+                        			{
+                            				n = f/d;
+                            				k = -e/d;
+                        			}
+                        			cout<<1<<" "<<k<<" "<<n;
+                    			}
+                		}
+            		}
+		}
+       		else
+        	{
+            		cout<<0;
+        	}
 	}
 	return 0;
 }
